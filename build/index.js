80de4059389177057795bf92326b1d9c005c126e
Benjamin <benng@cloude8.com>
2019-12-10 20:22:08 +0800

    chatbot app first commit

---
+++ build/index.js
@@ -0,0 +1,47 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const express_1 = __importDefault(require("express"));
+const API_KEY = "8c2230d";
+const app = express_1.default();
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
+app.get("/", (req, res) => {
+    res.send("Welcome to Movie API");
+});
+app.post("/get-movie-details", (req, res) => {
+    const movieToSearch = req.body.queryResult &&
+        req.body.queryResult.parameters &&
+        req.body.queryResult.parameters.movie
+        ? req.body.queryResult.parameters.movie
+        : "Joker";
+    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
+    axios_1.default
+        .get(reqUrl)
+        .then(responseFromAPI => {
+        const { Title, Year, Director, Actors, Plot } = responseFromAPI.data;
+        const dataToSend = `${Title} was released in the year ${Year}. It is directed by ${Director} and stars ${Actors}.\n Here some glimpse of the plot: ${Plot}`;
+        console.log(responseFromAPI.data);
+        return res.json({
+            fulfillmentText: dataToSend,
+            facebook: {
+                text: dataToSend
+            },
+            source: "get-movie-details"
+        });
+    })
+        .catch(error => {
+        console.log(error);
+        return res.json({
+            fulfillmentText: "Could not get results at this time",
+            source: "get-movie-details"
+        });
+    });
+});
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Server is up and running...");
+});
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+
+const API_KEY = "8c2230d";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Movie API");
+});
+
+app.post("/get-movie-details", (req: Request, res: Response) => {
+  const movieToSearch: string =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.movie
+      ? req.body.queryResult.parameters.movie
+      : "Joker";
+  const reqUrl = encodeURI(
+    `http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`
+  );
+
+  axios
+    .get(reqUrl)
+    .then(responseFromAPI => {
+      const { Title, Year, Director, Actors, Plot } = responseFromAPI.data;
+      const dataToSend = `${Title} was released in the year ${Year}. It is directed by ${Director} and stars ${Actors}.\n Here some glimpse of the plot: ${Plot}`;
+
+      console.log(responseFromAPI.data);
+      return res.json({
+        fulfillmentText: dataToSend,
+        facebook: {
+          text: dataToSend
+        },
+        source: "get-movie-details"
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      return res.json({
+        fulfillmentText: "Could not get results at this time",
+        source: "get-movie-details"
+      });
+    });
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server is up and running...");
+});
